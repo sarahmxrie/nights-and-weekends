95298f38568cdb8c4726c846af48e3c500fe3baf
Sarah <sarahmxrie.neumann@gmail.com>
2024-07-10 16:07:44 +0200

    Quartz sync: Jul 10, 2024, 4:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "subscribe": "https://eudaemonia.ck.page/b7d7859bc4",
     },
   }),
 }
